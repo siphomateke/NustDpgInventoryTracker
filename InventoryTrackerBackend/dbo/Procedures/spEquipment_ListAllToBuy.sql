ffcf71fe3e58667355a5a36fe4807c9b7491b217
Sipho Mateke <siphomateke@gmail.com>
2021-01-26 18:10:56 +0200

    Allow viewing all equipment which needs to be bought

---
+++ InventoryTrackerBackend/dbo/Procedures/spEquipment_ListAllToBuy.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[spEquipment_ListAllToBuy]
+	@UserId INT
+AS
+	DECLARE @CanApprove BIT;
+	EXEC spUser_HasPermission @UserId, 'CAN_APPROVE_NEW_EQUIPMENT', @CanApprove OUTPUT;
+
+	DECLARE @EquipmentToBuy TABLE (Name VARCHAR (50), Description VARCHAR (500), Quantity INT);
+
+	INSERT INTO @EquipmentToBuy
+	SELECT Name, Description, Quantity FROM v_EquipmentToBuy;
+
+	IF @CanApprove = 1
+		-- Also show all newly requested equipment from all users
+		INSERT INTO @EquipmentToBuy
+		EXEC spNewEquipment_List @UserId, 0;
+	ELSE
+		-- Also show all newly requested equipment from this user
+		INSERT INTO @EquipmentToBuy
+		EXEC spNewEquipment_List @UserId, 1;
---
+++ InventoryTrackerBackend/dbo/Views/v_EquipmentToBuy.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW [dbo].[v_EquipmentToBuy]
+	AS SELECT Equipment.* FROM Equipment	
+	INNER JOIN EquipmentCondition
+	ON (Equipment.ConditionId = EquipmentCondition.ConditionId)
+	WHERE Quantity = 0
+	OR Lost = 1
+	OR EquipmentCondition.Name = 'TERRIBLE';
