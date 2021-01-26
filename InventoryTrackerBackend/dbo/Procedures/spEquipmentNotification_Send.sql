65c0b4a6841bd9c560a2c41f430b4d5d772e0904
Sipho Mateke <siphomateke@gmail.com>
2021-01-26 18:09:46 +0200

    Send notifications when equipment changes
    E.g. stock low or damaged.

---
+++ InventoryTrackerBackend/dbo/Procedures/spEquipmentNotification_Send.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].[spEquipmentNotification_Send]
+	@EquipmentId INT,
+	@NotificationTypeName VARCHAR (50),
+	@Title VARCHAR (100) = NULL,
+	@Description VARCHAR (250) = NULL
+AS
+	-- Get notification type ID from name
+	DECLARE @NotificationTypeId INT;
+	SELECT @NotificationTypeId = EquipmentNotificationTypeId
+	FROM EquipmentNotificationType
+	WHERE Name = @NotificationTypeName;
+
+	-- Get equipment name for use in notification title
+	DECLARE @EquipmentName VARCHAR (50);
+	SELECT @EquipmentName = Name FROM Equipment WHERE EquipmentId = @EquipmentId;
+	IF @EquipmentName is NULL
+		SET @EquipmentName = 'Equipment';
+
+	-- Auto generate equipment notification titles from notification type and equipment name
+	IF @Title is NULL
+		IF @NotificationTypeName = 'NEEDS_REPAIR'
+			SET @Title = CONCAT(@EquipmentName + ' needs repairing');
+		ELSE IF @NotificationTypeName = 'OUT_OF_STOCK'
+			SET @Title = CONCAT(@EquipmentName + ' is out of stock');
+		ELSE IF @NotificationTypeName = 'RUNNING_LOW'
+			SET @Title = CONCAT(@EquipmentName + '''s stock is running low');
+	
+	-- Send notification to all users who can view the equipment and thus might be interested
+	INSERT INTO EquipmentNotification
+		(UserId, EquipmentId, Title, Description, NotificationTypeId)
+	SELECT 
+		UserViewableCategory.UserId, @EquipmentId, @Title, @Description, @NotificationTypeId
+	FROM EquipmentCategory
+	INNER JOIN UserViewableCategory
+	ON (UserViewableCategory.CategoryId = EquipmentCategory.CategoryId)
+	WHERE EquipmentCategory.EquipmentId = @EquipmentId;
---
+++ InventoryTrackerBackend/dbo/Triggers/TR_Equipment_AfterUpdate.sql
@@ -0,0 +1,34 @@
+﻿CREATE TRIGGER [TR_Equipment_AfterUpdate]
+ON Equipment
+AFTER UPDATE
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	-- Quantity level at which the user will be notified that the equipment's stock is running low.
+	DECLARE @MinQuanity INT = 3;
+
+	IF EXISTS(SELECT ConditionId FROM inserted) OR EXISTS(SELECT Quantity FROM inserted)
+		DECLARE @EquipmentId INT;
+		SELECT @EquipmentId = EquipmentId FROM inserted;
+
+		IF EXISTS(SELECT ConditionId FROM inserted)
+			-- Get name of condition
+			DECLARE @ConditionId INT; 
+			SELECT @ConditionId = ConditionId FROM inserted;
+			DECLARE @Condition VARCHAR (50);
+			SELECT @Condition = Name FROM EquipmentCondition WHERE ConditionId = @ConditionId;
+
+			IF @Condition = 'TERRIBLE'
+				EXEC spEquipmentNotification_Send @EquipmentId, 'NEEDS_REPAIR';
+
+		IF EXISTS(SELECT Quantity FROM inserted)
+			DECLARE @Quantity INT;
+			SELECT @Quantity = Quantity FROM inserted;
+
+			IF @Quantity < @MinQuanity
+				EXEC spEquipmentNotification_Send @EquipmentId, 'RUNNING_LOW';
+			ELSE IF @Quantity = 0
+				EXEC spEquipmentNotification_Send @EquipmentId, 'OUT_OF_STOCK';
+			
+END
