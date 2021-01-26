68a015ece11827a7585b8b350865c9283f5b5088
Sipho Mateke <siphomateke@gmail.com>
2021-01-26 15:58:37 +0200

    Auto approve changes done by admins

--- InventoryTrackerBackend/dbo/Triggers/TR_EquipmentChange_AfterInsert.sql
+++ InventoryTrackerBackend/dbo/Triggers/TR_EquipmentChange_AfterInsert.sql
@@ -8,27 +8,44 @@ BEGIN
 	DECLARE @NotificationTitle VARCHAR (100) = 'Equipment change';
 	DECLARE @NotificationMessage VARCHAR (250) = ' made changes to some equipment that require approval.';
 
-	-- Get username of user who changed equipment
-	DECLARE @Username VARCHAR(100);
-	SELECT TOP(1) @Username = Username FROM AppUser
-	WHERE AppUser.UserId = (SELECT ChangedByUserId FROM inserted);
+	DECLARE @ChangedByUserId INT;
+	SELECT @ChangedByUserId = ChangedByUserId FROM inserted;
 
-	-- If the user has since been deleted, use a different name for them in the notification message.
-	IF @Username IS NULL
-		SET @Username = '<missing user>';
+	-- If the user who authored the change can approve changes, auto approve the change
+	IF EXISTS(SELECT 1 FROM v_ApproverUsers WHERE UserId = @ChangedByUserId) BEGIN
+		DECLARE @EquipmentChangeId INT;
+		SELECT @EquipmentChangeId = EquipmentChangeId FROM inserted
 
-	-- Notify all users who can approve equipment change notifications
-	INSERT INTO EquipmentChangeNotification
-	(
-		[UserId],
-		[EquipmentChangeId],
-		[Title],
-		[Description]
-	)
-	SELECT 
-		v_ApproverUsers.UserId,
-		(SELECT EquipmentChangeId FROM inserted),
-		@NotificationTitle,
-		CONCAT(@Username, @NotificationMessage)
-	FROM v_ApproverUsers;
+		-- Immediately set equipment change as approved
+		EXEC spEquipmentChange_SetApproval 
+			@UserId = @ChangedByUserId, 
+			@EquipmentChangeId = @EquipmentChangeId,
+			@Approved = 1
+	END
+	-- If change author cannot approve changes, notify those who can
+	ELSE BEGIN
+		-- Get username of user who changed equipment
+		DECLARE @Username VARCHAR(100);
+		SELECT TOP(1) @Username = Username FROM AppUser
+		WHERE AppUser.UserId = @ChangedByUserId;
+
+		-- If the user has since been deleted, use a different name for them in the notification message.
+		IF @Username IS NULL
+			SET @Username = '<missing user>';
+
+		-- Notify all users who can approve equipment change notifications
+		INSERT INTO EquipmentChangeNotification
+		(
+			[UserId],
+			[EquipmentChangeId],
+			[Title],
+			[Description]
+		)
+		SELECT 
+			v_ApproverUsers.UserId,
+			(SELECT EquipmentChangeId FROM inserted),
+			@NotificationTitle,
+			CONCAT(@Username, @NotificationMessage)
+		FROM v_ApproverUsers;
+	END
 END
