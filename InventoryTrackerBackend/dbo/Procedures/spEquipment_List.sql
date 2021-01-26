c870e67be758e144aa41e062daabd8459baccd1d
Sipho Mateke <siphomateke@gmail.com>
2021-01-26 16:27:53 +0200

    Add procedure to get all equipment user can view

---
+++ InventoryTrackerBackend/dbo/Procedures/spEquipment_List.sql
@@ -0,0 +1,16 @@
+﻿-- Get list of all equipment a user can view
+CREATE PROCEDURE [dbo].[spEquipment_List]
+	@UserId INT
+AS
+	DECLARE @CanViewAny BIT;
+	EXEC spUser_CanViewAnyEquipment @UserId, @CanViewAny OUTPUT;
+
+	IF @CanViewAny = 1
+		SELECT Equipment.* FROM Equipment
+		INNER JOIN EquipmentCategory 
+		ON (Equipment.EquipmentId = EquipmentCategory.EquipmentId)
+		INNER JOIN UserViewableCategory
+		ON (UserViewableCategory.CategoryId = EquipmentCategory.CategoryId)
+		WHERE UserId = @UserId;
+	ELSE
+		RAISERROR('User does not have permission to view equipment', 16, 1);
