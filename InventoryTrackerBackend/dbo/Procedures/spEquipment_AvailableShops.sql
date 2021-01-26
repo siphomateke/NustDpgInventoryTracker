2df0029d49674c8d88bfda1dc2acd14cca9aabec
Sipho Mateke <siphomateke@gmail.com>
2021-01-26 18:11:16 +0200

    Allow checking where more of a certain equipment can be bought

---
+++ InventoryTrackerBackend/dbo/Procedures/spEquipment_AvailableShops.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[spEquipment_AvailableShops]
+	@UserId INT,
+	@EquipmentId INT
+AS
+	-- Only show information if user can view equipment
+	DECLARE @CanView BIT;
+	EXEC spUser_CanViewEquipment @UserId, @EquipmentId, @CanView OUTPUT;
+
+	IF @CanView = 1
+		SELECT [Shop].[ShopId], [Shop].[Name], [Shop].[PhoneNumber], [Shop].[Email], [Shop].[Comments], [Shop].[StreetAdress], [Shop].[Town], Country.Name as Country FROM Equipment
+		INNER JOIN EquipmentPricing
+		ON (EquipmentPricing.EquipmentId = Equipment.EquipmentId)
+		INNER JOIN Shop
+		ON (Shop.ShopId = EquipmentPricing.ShopId)
+		INNER JOIN Country
+		ON (Country.CountryId = Shop.CountryId)
+		WHERE Equipment.EquipmentId = @EquipmentId;
+	ELSE
+		RAISERROR('User does not have permission to view this equipment and its assocated information', 16, 1);
